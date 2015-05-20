a3bf872c3ddd7d789797fbd2aa29eefbd1d5fe5b
Brooke Patterson <brooke_patterson@cable.comcast.com>
2015-05-20 18:49:47 -0400

    added a distribution plot of the data

--- plot1.R
+++ plot1.R
@@ -6,11 +6,16 @@ SCC <- readRDS("../../data/exdata_data_NEI_data/Source_Classification_Code.rds")
 # Summarize the data by year
 byYear <- summaryBy(Emissions ~ year, data = NEI, FUN=c(length,mean,median,sum,sd))
 
-png("plot1.png")
+#png("plot1.png")
+windows(9,11,pointsize=1/100,bg="white")
+par(mfrow = c(2, 1))
+hist(x = log10(NEI$Emissions),
+     xlab = "Emissions",
+     main = "Distribution of all Emissions data")
 barplot(height = byYear$Emissions.sum, 
       names= byYear$year,
       main = "Sum Total of all PM2.5 Emisions By year",
       sub = "There is a year over year reduction",
       ylab = "PM2.5 Emissions in Tons",
       xlab = "Year")
-dev.off()
+savePlot('plot1.png',type="png")
