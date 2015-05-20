02e1eea65c764cd62c836aac444bc980cacb04c6
Brooke Patterson <brooke_patterson@cable.comcast.com>
2015-05-20 19:34:51 -0400

    plot4 successfully redone with a distribution plot for sanity

--- plot4.R
+++ plot4.R
@@ -1,5 +1,6 @@
 library(doBy)
 library(ggplot2)
+library(gridExtra)
 
 NEI <- readRDS("../../data/exdata_data_NEI_data/summarySCC_PM25.rds")
 SCC <- readRDS("../../data/exdata_data_NEI_data/Source_Classification_Code.rds")
@@ -14,10 +15,17 @@ byYearCoal <- summaryBy(Emissions ~ year + EI.Sector,
                              data = NEIcoal,
                              FUN=c(length,mean,median,sum,sd))
 
+q <- ggplot(aes(x=log10(Emissions), colour=EI.Sector, group=EI.Sector), data=NEIcoal)
+q <- q + geom_density(fill=NA)
+q <- q + ggtitle("Distribution of Emissions for coal combustion")
+
 p <- ggplot(data=byYearCoal, aes(x=year, y=log10(Emissions.sum), group=EI.Sector))
 p <- p + geom_line(aes(colour = EI.Sector))
 p <- p + ggtitle("Emissions by year for coal combustion")
 p <- p + xlab("Year")
 p <- p + ylab("PM2.5 Emission in Tons (log10)")
 p
-ggsave(p,file="plot4.png",scale=1.5)
+
+r <- arrangeGrob(q,p)
+
+ggsave(r,file="plot4.png",scale=1.5)
